f033e14ad20ff3e39c6a4fdf40eabadb25f02cb4
mharn29 <pasicolanjaycell@gmail.com>
2023-02-27 17:13:38 +0800

    exercise 4: Get Name by given age

---
+++ exercise/exercise_4.rb
@@ -0,0 +1,11 @@
+def find_name(age_value)
+  names = { john: 18, lester: 31, juan: 24 }
+
+  names.each do |name, age|
+    if age == age_value
+      return name
+    end
+  end
+end
+
+p find_name(24)
